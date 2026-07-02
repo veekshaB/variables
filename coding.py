d7aafae0396a6c392356dfb0ce04adda102a74f4
veekshaB <pengunti@gmail.com>
2026-07-02 18:59:34 -0400

    Add files via upload

---
+++ coding.py
@@ -0,0 +1,4 @@
+print("welcome to the world of programing.")
+print(7)
+print("my name is Veeksha.\n I am 11 years old")
+print("how many baskets are there in the kitchen",5)
---
+++ key words.py
@@ -0,0 +1,5 @@
+import keyword
+print(keyword.kwlist)
+name=input("enter your name")
+age=input("enter your age")
+gender=input("enter your gender")
